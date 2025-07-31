80b9ffed784bc9a07e912f8a28e46ca9771d8afb
kmj123 <kkk990108@naver.com>
2025-07-31 11:47:48 +0900

    학생성적 프로그램

---
+++ Ja0731/src/StuMain.java
@@ -0,0 +1,70 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StuMain {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		ArrayList list = new ArrayList();
+		String name = null;
+		int no,kor,eng,math,toal = 0;
+		double avg = 0;
+		String [] title = {
+				"번호","이름","국어","영어","수학","합계","평균","등수"
+				};
+//		Stuscore[] s = new Stuscore[10];
+		Stuscore s = null;
+		
+		while(true) {
+			System.out.println("[ 학생성적 프로그램 ]");
+			System.out.println("1. 성적입력");
+			System.out.println("2. 성적출력");
+			System.out.println("3. 성적수정");
+			System.out.println("4. 성적삭제");
+			System.out.println("5. 등수처리");
+			System.out.println("6. 이름정렬");
+			System.out.println("0. 프로그램 종료");
+			System.out.println("--------------------------");
+			System.out.println("원하는 번호를 입력하세요>> ");
+			int choice = scan.nextInt();
+			
+			switch(choice) {
+			case 1:	// 성적입력 - 이름, 국어, 영어, 수학 수동입력 -> 번호, 합계, 평균 자동입력
+				System.out.printf("%d번째 학생 이름을 입력하세요 \n", list.size()+1);
+				name = scan.next();
+				System.out.println("국어점수를 입력하세요>>");
+				kor = scan.nextInt();
+				System.out.println("영어점수를 입력하세요>>");
+				eng = scan.nextInt();
+				System.out.println("수학점수를 입력하세요>>");
+				math = scan.nextInt();
+				// 컬렉션 저장
+				list.add(new Stuscore(name,kor,eng,math)); 
+				System.out.printf("%s 학생성적이 저장되었습니다.\n",name);
+				break;
+			case 2: 
+				System.out.println("		      [ 학생성적 출력 ]"); 	
+				System.out.printf(""
+						+"%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n",
+						title[0],title[1],title[2],title[3],title[4],
+						title[5],title[6],title[7]
+						);
+				System.out.println("----------------------------------------------------------------");
+				for(int i=0;i<list.size();i++) {
+					s = (Stuscore)list.get(i);	//Object- > Stuscore
+					System.out.printf("%d\t%s\t%d\t%d\t%d\t%d\t%.2f\t%d\n",
+							s.getNo(),s.getName(),s.getKor(),s.getEng(),
+							s.getMath(),s.getTotal(),s.getAvg(),s.getRank()
+							);
+				}
+				break;
+			case 3: 
+				break;
+			}
+			
+		}	// while
+
+		
+	}
+
+}
---
+++ Ja0731/src/Stuscore.java
@@ -0,0 +1,83 @@
+
+public class Stuscore {
+	// private으로 만든 경우 getter setter 필수
+	// 같은 클래스내에서만 사용가능
+	public static int count;
+	private int no;
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int total;
+	private double avg;
+	private int rank;
+	
+	// 초기화 블럭 - 객체 생성시 우선 실행
+	// no 자동 증가 
+	{
+		no = ++count;
+	}
+	
+	// 기본생성자
+	Stuscore(){}
+	Stuscore(String name, int kor, int eng, int math){
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.total = kor+eng+math;
+		this.avg = this.total/3.0;
+	}
+	
+	// Getter/ Setter
+	public int getNo() {
+		return no;
+	}
+	public void setNo(int no) {
+		this.no = no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getKor() {
+		return kor;
+	}
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+	public int getEng() {
+		return eng;
+	}
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+	public int getMath() {
+		return math;
+	}
+	public void setMath(int math) {
+		this.math = math;
+	}
+	public int getTotal() {
+		return total;
+	}
+	public void setTotal(int total) {
+		this.total = total;
+	}
+	public double getAvg() {
+		return avg;
+	}
+	public void setAvg(double avg) {
+		this.avg = avg;
+	}
+	public int getRank() {
+		return rank;
+	}
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+	
+	
+} 
